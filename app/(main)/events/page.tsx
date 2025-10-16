5d69ab7c36be96f90b2aa54a4c550472530f765c
Aswin-04 <e.aswin004@gmail.com>
2025-10-16 10:39:03 +0530

    seperate events by section

--- app/(main)/events/page.tsx
+++ app/(main)/events/page.tsx
@@ -1,22 +1,59 @@
-import React from 'react'
-import { eventsData } from '@/lib/constants'
-import EventCard from '@/components/event-card'
+import React from "react";
+import { technicalEvents, nonTechnicalEvents, workshops } from "@/lib/constants";
+import EventCard from "@/components/event-card";
 
 const Events = () => {
   return (
     <section className="pt-[133px]">
-      <div className='padding-b max-container' >
-        <h2 className='my-10 h1 gradient-primary tracking-tight text-center ' >Events</h2>
-        <ul className='flex flex-wrap justify-center' >
-          {eventsData.map((event) => (
-            <li key={event.id}>
-              <EventCard event={event} ></EventCard>
-            </li>
-          ))}
-        </ul>
+      <div className="padding-b  padding-x max-container">
+        <h2 className="my-10 text-4xl md:text-[44px] font-sans font-bold tracking-tight gradient-primary text-center uppercase">
+          Events
+        </h2>
+
+        {/* === TECHNICAL EVENTS === */}
+        <div className="my-16">
+          <h2 className="text-3xl md:text-[36px] uppercase font-sans font-semibold tracking-tight gradient-primary mb-8 text-center">
+            Technical Events
+          </h2>
+          <ul className="flex flex-wrap justify-center gap-6">
+            {technicalEvents.map((event) => (
+              <li key={event.id}>
+                <EventCard event={event} />
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        {/* === NON-TECHNICAL EVENTS === */}
+        <div className="my-16">
+          <h2 className="text-3xl md:text-[36px] uppercase font-sans font-semibold tracking-tight gradient-primary mb-8 text-center">
+            Non-Technical Events
+          </h2>
+          <ul className="flex flex-wrap justify-center gap-6">
+            {nonTechnicalEvents.map((event) => (
+              <li key={event.id}>
+                <EventCard event={event} />
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        {/* === WORKSHOPS === */}
+        <div className="my-16">
+          <h2 className="text-3xl md:text-[36px] uppercase font-sans font-semibold tracking-tight gradient-primary mb-8 text-center">
+            Workshops
+          </h2>
+          <ul className="flex flex-wrap justify-center gap-6">
+            {workshops.map((event) => (
+              <li key={event.id}>
+                <EventCard event={event} />
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
+    </section>
+  );
+};
 
-    </section>  )
-}
-
-export default Events
+export default Events;
--- lib/constants.ts
+++ lib/constants.ts
@@ -109,6 +109,362 @@ export const quickLinks = {
   ],
 };
 
+export const technicalEvents = [
+  {
+    id: 1,
+    logo: techscriptLogo,
+    title: "TechScript",
+    description:
+      "Present innovative engineering solutions addressing real-world challenges through engaging presentations and Q&A sessions.",
+    guidelines: [
+      "Team size: 2 – 3 members.",
+      "Selection will be on a first-come, first-served basis.",
+      "Teams will have a presentation duration of 5 minutes and Q&A session of 2 minutes.",
+      "Participants must send their presentation PPT on or before 25.10.2025.",
+      "Participants must assemble in the mentioned venue on time."
+    ],
+    generalInstructions: [
+      {
+        title: "1. PRESENTATION GUIDELINES",
+        points: [
+          "Time Limit: Each team/participant will have 7 minutes for the presentation followed by 2 minutes for Q&A.",
+          "Format: The presentation should be in PowerPoint (PPT) format, and one slide must contain Business model of your topic.",
+          "Prepare a number of slides based on the given time limit."
+        ]
+      },
+      {
+        title: "2. TECHNICAL REQUIREMENTS",
+        points: [
+          "Participants must send a copy of their presentation in advance through mail.",
+          "System and Projector Facility will be arranged for you. Bring of your laptop is highly appreciable.",
+          "If using special software or videos, inform the organizers beforehand."
+        ]
+      },
+      {
+        title: "3. SUBMISSION & FORMATTING",
+        points: [
+          "Abstract word limit: 250–300 words",
+          "Format: Times New Roman, 12 pt, 1.5 line spacing",
+          "Presentation medium: PPT (7–10 slides maximum)",
+          "Time: 10 minutes (5 – 7 mins presentation + 2 min Q&A)"
+        ]
+      }
+    ],
+    eventCoordinators: [
+      { image: kesavanPic, name: "KESAVAN S", contact: "8148864524" },
+      { image: dhanavanthiriPic, name: "DHANAVANTHIRI B", contact: "8015792388" }
+    ],
+    notes: [
+      "The confirmation mail will be sent to participants, and they will be added to a WhatsApp group for communication.",
+      "Please note that the registration for this event is handled through a common Google Form.",
+      "Stay tuned for important information and instructions shared via email and WhatsApp.",
+      "You can click Go Back to return to the events page or Register Now to proceed with registration."
+    ]
+  },
+  {
+    id: 2,
+    logo: curseOfCadLogo,
+    title: "Curse of CAD",
+    description:
+      "Unleash your creativity and engineering skills in the Curse of CAD. Participants will design a complex object or assembly using SOLIDWORKS within a specific time frame. It’s a test of design principles, accuracy, and execution.",
+    guidelines: [
+      "Participants must compete individually.",
+      "A 3D model will be given, and you must complete it within 1 hour using SOLIDWORKS software.",
+      "There will be two rounds, and winners will be chosen based on accuracy and time management.",
+      "Any form of malpractice will result in disqualification.",
+      "PC will be provided to participants; you may also bring your own laptop."
+    ],
+    eventCoordinators: [
+      { image: thiyaguPic , name: "THIYAGARAJAN S", contact: "9092552423" },
+      { image: mharishPic, name: "HARISH M", contact: "7639011009" }
+    ],
+    notes: [
+      "The confirmation mail will be sent to participants, and they will be added to a WhatsApp group for communication.",
+      "Please note that the registration for this event is handled through a common Google Form.",
+      "Stay tuned for important information and instructions shared via email and WhatsApp.",
+      "You can click Go Back to return to the events page or Register Now to proceed with registration."
+    ]
+  },
+  {
+    id: 3,
+    logo: rcDaCorsaLogo,
+    title: "RC Da Corsa",
+    description:
+      "Race your remote-controlled cars across challenging tracks in this thrilling RC Racing event testing control, speed, and precision.",
+    guidelines: [
+      "Team size: 2 – 4 members.",
+      "Selection will be on a first-come, first-served basis.",
+      "Initial inspection will be done before the race.",
+      "Each penalty will cause you an additional time.",
+      "Participants are fully responsible for their own RC car.",
+      "Dimensions: 30 cm × 30 cm × 30 cm; Weight: 2 – 5 kg."
+    ],
+    generalInstructions: [
+      {
+        title: "",
+        points: [
+          "Only one participant can control the car at a time.",
+          "The race will generally be a time trial or circuit race where the RC car completes laps or a set course in the shortest time possible.",
+          "The RC car must be fully remote controlled during the race; no physical assistance except by designated marshals.",
+          "If the car overturns or goes off track, marshals may place it back on the track with time penalties applied.",
+          "Cars must not leave any disintegrated parts on the track or deliberately damage opponents' vehicles.",
+          "The car must comply with size, weight, and technical specifications as defined by the event organizers (e.g., around 30 × 30 × 30 cm, max 5 kg).",
+          "The track will have checkpoints; completing laps requires crossing these checkpoints.",
+          "Drivers must remain in designated driver areas and should not run alongside the car on the track.",
+          "Time penalties apply for touching the car, boundary violations, skipping obstacles.",
+          "Obstacle challenges may include ramps, rough terrain, and maybe water/fire etc., for testing control and durability of RC Car.",
+          "Any damage to track leads to penalty."
+        ]
+      }
+    ],
+    eventCoordinators: [
+      { image: muthuPic, name: "MUTHUKUMARAN V", contact: "8072844918" },
+      { image: sanjayPic, name: "SANJAY MATHAV M", contact: "8148743882" },
+      { image: nithyaPic, name: "NITHYANANDAM M", contact: "9159160194" }
+    ],
+    notes: [
+      "The confirmation mail will be sent to participants, and they will be added to a WhatsApp group for communication.",
+      "Please note that the registration for this event is handled through a common Google Form.",
+      "Stay tuned for important information and instructions shared via email and WhatsApp.",
+      "You can click Go Back to return to the events page or Register Now to proceed with registration."
+    ]
+  },
+  {
+    id: 4,
+    logo: projectExpoLogo,
+    title: "Project Expo",
+    description:
+      "A celebration of innovation, sustainability, and engineering brilliance where students present projects aligned with the UN Sustainable Development Goals (SDGs).",
+    guidelines: [
+      "Each team must have 2–4 members.",
+      "Cross-year collaboration is allowed but participants must be from the same institution.",
+      "Internet-based or copied projects will be disqualified.",
+      "One member must be designated as the Team Leader.",
+      "Participants must carry College ID cards.",
+      "Teams must maintain professionalism and discipline.",
+      "The decision of judges is final and binding."
+    ],
+    generalInstructions: [
+      {
+        title: "1. PROJECT REQUIREMENTS",
+        points: [
+          "Projects must address at least any one SDG.",
+          "Working models/prototypes are strongly encouraged. Conceptual designs will also be accepted with proper justification.",
+          "Each team must prepare a PPT (max 10 pages) covering:",
+          "Title & Problem Statement",
+          "Objective & SDG Linkage",
+          "Methodology / Design",
+          "Expected Impact",
+          "Conclusion"
+        ]
+      },
+      {
+        title: "2. PRESENTATION GUIDELINES",
+        points: [
+          "Time: 7 minutes presentation + 3 minutes Q&A.",
+          "Mode: PPT/Prototype/Poster display.",
+          "Teams must rehearse for smooth delivery and stick to the time limit.",
+          "Participants must send their presentation PPT on or before 26.10.2025."
+        ]
+      }
+    ],
+    eventCoordinators: [
+      { image: sivaramanPic, name: "SIVARAMAN M", contact: "8925202427" },
+      { image: aharishPic, name: "HARISH A", contact: "9344533357" }
+    ],
+    notes: [
+      "The confirmation mail will be sent to participants, and they will be added to a WhatsApp group for communication.",
+      "Please note that the registration for this event is handled through a common Google Form.",
+      "Stay tuned for important information and instructions shared via email and WhatsApp.",
+      "You can click Go Back to return to the events page or Register Now to proceed with registration."
+    ]
+  }
+];
+
+
+export const nonTechnicalEvents = [
+  {
+    id: 5,
+    logo: adClashLogo,
+    title: "AD Clash",
+    description:
+      "A fun-filled and creative marketing event where teams conceptualize and perform live advertisements for random products.",
+    guidelines: [
+      "Each team should consist of 2 – 4 members.",
+      "Each team must report 15 to 30 minutes before the event for product allotment.",
+      "Random products will be given to the team before their performance.",
+      "Maximum time limit for each team performance is 10 minutes.",
+      "Teams will be evaluated based on creativity, presentation, entertainment value and advertisement strategy.",
+      "Use of offensive language, obscene gestures or false claims is strictly prohibited and will lead to disqualification."
+    ],
+    eventCoordinators: [
+      { image: arjunPic, name: "ARJUN P B", contact: "9524007686" },
+      { image: abineshPic, name: "ABINESH J", contact: "8838338464" }
+    ],
+    notes: [
+      "The confirmation mail will be sent to participants, and they will be added to a WhatsApp group for communication.",
+      "Please note that the registration for this event is handled through a common Google Form.",
+      "Stay tuned for important information and instructions shared via email and WhatsApp.",
+      "You can click Go Back to return to the events page or Register Now to proceed with registration."
+    ]
+  },
+  {
+    id: 6,
+    logo: memechanicsLogo,
+    title: "Memechanics",
+    description:
+      "A meme-making competition celebrating humor, creativity, and engineering culture.",
+    guidelines: [
+      "Each team should consist of 2 members.",
+      "This event will be conducted online.",
+      "Participants must create a meme based on the given theme or topic.",
+      "Meme should be fun, creative, entertaining, and respectful.",
+      "Meme must not contain any vulgarity, offensive, political, or religiously sensitive content.",
+      "Judging will be based on creativity, humor, originality, and relevance to the theme given.",
+      "Topics: Engineering Reloaded, Campus Chronicles, Hostel Diaries, Classroom Confidential, Canteen Tales."
+    ],
+    eventCoordinators: [
+      { image: anandPic, name: "ANANDHARAJA S", contact: "9500792433" },
+      { image: giriPic, name: "GIRIDHARAN V", contact: "6374305761" }
+    ],
+    notes: [
+      "The confirmation mail will be sent to participants, and they will be added to a WhatsApp group for communication.",
+      "Please note that the registration for this event is handled through a common Google Form.",
+      "Stay tuned for important information and instructions shared via email and WhatsApp.",
+      "You can click Go Back to return to the events page or Register Now to proceed with registration."
+    ]
+  },
+  {
+    id: 7,
+    logo: assemblexLogo,
+    title: "AssembleX",
+    description:
+      "A mechanical assembly challenge where participants must assemble given components correctly within the time limit.",
+    guidelines: [
+      "Each team should consist of 2 participants.",
+      "Participants must complete the assembly within the given time.",
+      "The parts should be assembled in the correct order and orientation as per the given instructions/model.",
+      "Do not bend, break, or damage the mechanical parts provided. Any intentional damage may lead to disqualification.",
+      "Teams are not allowed to exchange or borrow parts from other teams.",
+      "Arguments, misbehaviour, or disturbing other participants will not be tolerated. Violating teams will be disqualified."
+    ],
+    eventCoordinators: [
+      { image: akashPic, name: "SREE AKASH KUMAR S", contact: "9043657272" },
+      { image: nandhaPic, name: "NANDHAKUMAR N", contact: "6381302542" }
+    ],
+    notes: [
+      "The confirmation mail will be sent to participants, and they will be added to a WhatsApp group for communication.",
+      "Please note that the registration for this event is handled through a common Google Form.",
+      "Stay tuned for important information and instructions shared via email and WhatsApp.",
+      "You can click Go Back to return to the events page or Register Now to proceed with registration."
+    ]
+  },
+  {
+    id: 8,
+    logo: creepyClipsLogo,
+    title: "Creepy Clips",
+    description:
+      "A short-reel event celebrating the emotions, creativity, and energy of campus life through visuals and storytelling.",
+    guidelines: [
+      "Each team consists of 1 – 2 members.",
+      "Participants should capture the campus infrastructure, students, events, nature, and symposium vibes.",
+      "Captured reel or video time should be maximum 1 minute.",
+      "Participants may use filters, music, and captions to enhance their creativity.",
+      "Maintain decorum while filming and avoid disturbing classes or events.",
+      "Reels will be judged based on creativity, video quality, and editing."
+    ],
+    eventCoordinators: [
+      { image: jananiPic, name: "JANANI G", contact: "9585579223" },
+      { image: sriDharshiniPic, name: "SHRI DHARSHANI P", contact: "9094801330" }
+    ],
+    notes: [
+      "The confirmation mail will be sent to participants, and they will be added to a WhatsApp group for communication.",
+      "Please note that the registration for this event is handled through a common Google Form.",
+      "Stay tuned for important information and instructions shared via email and WhatsApp.",
+      "You can click Go Back to return to the events page or Register Now to proceed with registration."
+    ]
+  },
+  {
+    id: 9,
+    logo: knightAuctionLogo,
+    title: "Knights Auction",
+    description:
+      "An IPL-style cricket auction event where participants act as franchise owners, bidding strategically to build their dream team.",
+    guidelines: [
+      "Each team consists of two members.",
+      "Each team will be provided with a budget of Rs.100 crores.",
+      "Each team must select 16 to 20 players for their squad, following the given rules.",
+      "The squad must consist of 5 to 6 batsmen, 5 to 6 bowlers, 4 to 5 all-rounders, and 2 to 3 wicketkeepers.",
+      "A team can have a maximum of six foreign players.",
+      "At the end of the auction, the playing XI should have 4 batsmen, 3 bowlers, 3 all-rounders, and 1 wicketkeeper.",
+      "Winners will be decided based on the total credit points of the playing XI.",
+      "Violating any rule will lead to immediate disqualification."
+    ],
+    eventCoordinators: [
+      { image: kawinPic, name: "KAWIN HARSHAN K", contact: "9176602306" },
+      { image: rishiPic, name: "RISHIKESHWARAN R H", contact: "7904124281" }
+    ],
+    notes: [
+      "The confirmation mail will be sent to participants, and they will be added to a WhatsApp group for communication.",
+      "Please note that the registration for this event is handled through a common Google Form.",
+      "Stay tuned for important information and instructions shared via email and WhatsApp.",
+      "You can click Go Back to return to the events page or Register Now to proceed with registration."
+    ]
+  }
+];
+
+export const workshops = [
+  {
+    id: 10,
+    logo: tdpLogo,
+    title: "3D Printing Workshop",
+    description:
+      "A hands-on workshop exploring 3D printing technology, from design to fabrication, guided by expert instructors.",
+    guidelines: [
+      "Limited seats available – selection is strictly first come, first served.",
+      "Online registration is mandatory to confirm participation.",
+      "Participants must carry their college ID on the day of the workshop.",
+      "Be punctual and attend the full session to receive your certificate.",
+      "Maintain discipline and decorum throughout the event."
+    ],
+    eventCoordinators: [
+      { image: bashaPic, name: "GNAMATH BASHA A", contact: "9600159097" },
+      { image: suryaPic, name: "SURYA A", contact: "6380530806" }
+    ],
+    notes: [
+      "The confirmation mail will be sent to participants, and they will be added to a WhatsApp group for communication.",
+      "Please note that the registration for this event is handled through a common Google Form.",
+      "Stay tuned for important information and instructions shared via email and WhatsApp.",
+      "You can click Go Back to return to the events page or Register Now to proceed with registration."
+    ]
+  },
+  {
+    id: 11,
+    logo: evLogo,
+    title: "EV Technology Workshop",
+    description:
+      "A practical workshop exploring the technology behind electric vehicles, including batteries, motors, and charging systems.",
+    guidelines: [
+      "Online registration is mandatory to confirm your participation.",
+      "Limited seats available – allocation will be done on a first-come, first-served basis.",
+      "Participants must carry their college ID card on the day of the workshop.",
+      "Be punctual and attend the full session to receive your certificate.",
+      "Maintain discipline and decorum throughout the event."
+    ],
+    eventCoordinators: [
+      { image: santhoshPic, name: "SANTHOSH KUMAR P", contact: "6374903071" },
+      { image: abishekPic, name: "KARUPU ABISHEK M", contact: "9384954085" }
+    ],
+    notes: [
+      "The confirmation mail will be sent to participants, and they will be added to a WhatsApp group for communication.",
+      "Please note that the registration for this event is handled through a common Google Form.",
+      "Stay tuned for important information and instructions shared via email and WhatsApp.",
+      "You can click Go Back to return to the events page or Register Now to proceed with registration."
+    ]
+  }
+]
+
+
 export const eventsData = [
   {
     id: 1,
